b740198bbab3bc5fc18643988b822d67b65753b9
Bagus Prabangkoro (babang) <bagus.prabangkoro@gdn-commerce.com>
2019-09-20 18:28:52 +0700

    place submit method on parent component

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react'
 import SearchBar from './SearchBar'
 
 export default class App extends Component {
+  onFormSubmit (text) {
+    console.log(text)
+  }
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onSubmit={this.onFormSubmit} />
       </div>
     )
   }
