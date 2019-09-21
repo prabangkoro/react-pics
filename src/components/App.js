6cf6b9bdb691baa008388fc9c567d29966ee40f8
bagus prabangkoro <bagusprabangkoro@yahoo.co.id>
2019-09-21 09:21:02 +0700

    invoke method from child

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react'
 import SearchBar from './SearchBar'
 
 export default class App extends Component {
+  onSearchSubmit = text => {
+    console.log(text)
+  }
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     )
   }
